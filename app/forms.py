b207c86ea42f6beee4cce3aff144aadf0418c67a
Liam Melody <liammelody05@gmail.com>
2025-10-01 15:46:29 -0400

    chapter6

--- app/forms.py
+++ app/forms.py
@@ -15,6 +15,12 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Sign In")
 
 
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
+
+
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -33,8 +39,3 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError("Please use a different email address.")
-
-    class EditProfileForm(FlaskForm):
-        username = StringField("Username", validators=[DataRequired()])
-        about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
-        submit = SubmitField("Submit")
