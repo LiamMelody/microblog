0630d573c675c9c34fa7ffb6571ae5963eac3631
Liam Melody <liammelody05@gmail.com>
2025-12-01 10:23:30 -0500

    Chapter 21

--- app/forms.py
+++ app/forms.py
@@ -86,3 +86,10 @@ class PostForm(FlaskForm):
         _l("Say something"), validators=[DataRequired(), Length(min=1, max=140)]
     )
     submit = SubmitField(_l("Submit"))
+
+
+class MessageForm(FlaskForm):
+    message = TextAreaField(
+        _l("Message"), validators=[DataRequired(), Length(min=0, max=140)]
+    )
+    submit = SubmitField(_l("Submit"))
